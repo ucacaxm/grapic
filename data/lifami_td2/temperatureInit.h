97d880d8693a81a48a845c079e7213b35a940a58
Alexandre Meyer <alexandre.meyer@univ-lyon1.fr>
2017-10-10 16:04:35 +0200

    ...

---
+++ data/lifami_td2/temperatureInit.h
@@ -0,0 +1,11 @@
+void initFromData(WorldTemperature& wt)
+{
+    wt.n = 7;
+	wt.ws[0].lon = -25.000000;	wt.ws[0].lat = -85.000000;	wt.ws[0].temperature = -27;
+	wt.ws[1].lon = -97.000000;	wt.ws[1].lat = -35.000000;	wt.ws[1].temperature = 9.890400;
+	wt.ws[2].lon = 11.000000;	wt.ws[2].lat = -15.000000;	wt.ws[2].temperature = 25.130000;
+	wt.ws[3].lon = 125.000000;	wt.ws[3].lat =  13.000000;	wt.ws[3].temperature = 33.71700;
+	wt.ws[4].lon = -97.000000;	wt.ws[4].lat =   1.000000;	wt.ws[4].temperature = 32.89800;
+	wt.ws[5].lon = 19.000000;	wt.ws[5].lat =  41.000000;	wt.ws[5].temperature = -10.39100;
+	wt.ws[6].lon = -89.000000;	wt.ws[6].lat =  63.000000;	wt.ws[6].temperature = -17.466800;
+}
--- script/make_zip.sh
+++ script/make_zip.sh
@@ -1,74 +1,91 @@
 #!/bin/bash
 
-APPS="apps"
-GRAPIC="grapic.github"
-VERSION=`cat doc/VERSION`
-SAVE="../grapic-${VERSION}"
-TOCOPY="apps bin build data script extern src Makefile premake4.lua README.md"
+pause()
+{
+	read -p "Press [Enter] to continue..."
+}
 
+
+APPS="apps"
+#GRAPIC="grapic.github"
+GRAPIC=`pwd`
+VERSION=`cat doc/VERSION`
+SAVE_SHORT="grapic-${VERSION}"
+SAVE="${GRAPIC}/../${SAVE_SHORT}"
+TOCOPY="apps bin build data script extern src Makefile grapic.lua premake4.lua premake.bat README.md"
+
+TOZIP_SHORT="apps data src Makefile grapic.lua premake4.lua README.md"
+TOZIP_LINUX_SHORT="script/premake4.linux build/linux"
+TOZIP_MINGW_SHORT="extern/mingw bin/*.dll script/premake4.exe build/windows"
+TOZIP_MAC_SHORT="$extern/macosx script/premake4.macos script/premake5.macos build/macos"
+TOZIP_VISUAL2015_SHORT="extern/visual2015 premake-visual.bat script/premake5.exe script/premake4.exe build/windows"
+
+
+TOZIP=`for i in ${TOZIP_SHORT};do printf "${SAVE_SHORT}/$i ";done`
+TOZIP_LINUX=`for i in ${TOZIP_LINUX_SHORT};do printf "${SAVE_SHORT}/$i ";done`
+TOZIP_MINGW=`for i in ${TOZIP_MINGW_SHORT};do printf "${SAVE_SHORT}/$i ";done`
+TOZIP_MAC=`for i in ${TOZIP_MAC_SHORT};do printf "${SAVE_SHORT}/$i ";done`
+TOZIP_VISUAL2015=`for i in ${TOZIP_VISUAL2015_SHORT};do printf "${SAVE_SHORT}/$i ";done`
+
+
+echo "GRAPIC=${GRAPIC}"
+echo "GRAPIC-ZIP=${SAVE}"
+echo "TOZIP_SHORT=${TOZIP_SHORT}"
+echo "TOZIP=${TOZIP}"
+echo "TOZIP_LINUX=${TOZIP_LINUX}"
+echo "TOZIP_MINGW=${TOZIP_MINGW}"
+echo "TOZIP_MAC=${TOZIP_MAC}"
+echo "TOZIP_VISUAL2015=${TOZIP_VISUAL2015}"
+pause
 #ZIP="/c/Program\ Files/7-Zip/7z.exe"
 ZIP="zip"
 
 echo "=============================> CREATE COPY WITH VERSION: ${SAVE}"
-make clean
-mkdir ../${SAVE}
+cd ${GRAPIC}
+make bin/remove_correction.exe
+rm -rf ${SAVE}
+mkdir ${SAVE}
 cp -rf ${TOCOPY} ${SAVE} 
+pause 
 
 echo "=============================> REMOVE CORRECTION: ${APPS}"
 cd ${SAVE}
-find apps -name "*.cpp" -exec bin/remove_correction.exe {} {} \;
+find apps -name "*.cpp" -exec ${GRAPIC}/bin/remove_correction.exe {} {} \;
 
 
 
-REMOVE="-x ${GRAPIC}/.git\* \
-		-x ${GRAPIC}/obj\* \
-		-x ${GRAPIC}/build-cmake\* \
-		-x ${GRAPIC}/doc\* \
-		-x ${GRAPIC}/doc/download\* \
-		-x ${GRAPIC}/doc/html\* \
-		-x ${GRAPIC}/doc/images/old\*  \
-		-x ${GRAPIC}/bin/\*.exe \
-		-x ${GRAPIC}/tmp\* \
-		-x ${GRAPIC}/apps/zzz\** \
-		-x ${GRAPIC}/data/zzz\** \
-		-x ${GRAPIC}/premake-beta.bat \
-		-x ${GRAPIC}/extern/visual2013\* \
-		-x ${GRAPIC}/bin/*.ilk \
-		"
 
-REMOVE_MINGW="-x ${GRAPIC}/extern/mingw\*"
-REMOVE_MAC="-x ${GRAPIC}/extern/macosx\*"
-REMOVE_VISUAL2015="-x ${GRAPIC}/extern/visual2015\* \
-					-x ${GRAPIC}/premake-visual.bat \
-					-x ${GRAPIC}/script/premake5.\*"
 
 		
 makezip () 
 {
-	#pwd
-	#echo "==> ${ZIP} a $1 ../${GRAPIC}"
 	cd ${SAVE}/..
-	eval ${ZIP} -r $1 ${SAVE} ${REMOVE} $2 $3 $4
-										
-	
+	rm $1
+	echo "In `pwd`"
+	RUN="${ZIP} -q -r $1 $2 $3"
+	echo $RUN
+	eval ${RUN}
 	cd ${GRAPIC}
+	pause
 }
 
 echo "=============================> MINGW win"
 FILE="${GRAPIC}/doc/download/grapic-${VERSION}_win.zip"
-makezip "${FILE}" "${REMOVE_MAC}" "${REMOVE_VISUAL2015}" ""
+makezip "${FILE}" "${TOZIP}" "${TOZIP_MINGW}"
 echo "=============================> ZIP mac"
 FILE="${GRAPIC}/doc/download/grapic-${VERSION}_mac.zip"
-makezip "${FILE}" "${REMOVE_MINGW}" "${REMOVE_VISUAL2015}" ""
+makezip "${FILE}" "${TOZIP}" "${TOZIP_MAC}"
 echo "=============================> ZIP VISUAL2015"
 FILE="${GRAPIC}/doc/download/grapic-${VERSION}_visual2015.zip"
-makezip "${FILE}" "${REMOVE_MINGW}" "${REMOVE_MAC}" ""
-echo "=============================> TGZ"
+makezip "${FILE}" "${TOZIP}" "${TOZIP_VISUAL2015}"
+echo "=============================> LINUX TGZ"
 FILETAR="${GRAPIC}/doc/download/grapic-${VERSION}.tar"
 FILETGZ="${GRAPIC}/doc/download/grapic-${VERSION}.tgz"
-makezip "${FILETAR}" "${REMOVE_MINGW}" "${REMOVE_MAC}" "${REMOVE_VISUAL2015}"
-eval ${ZIP} a ${FILETGZ} ${FILETAR}
-rm -rf ${FILETAR}
+makezip "${FILE}" "${TOZIP}" "${TOZIP_LINUX}"
+gzip -f ${FILETAR}
+#rm -rf ${FILETAR}
+
+pause
 
 cd ${GRAPIC}
 rm -rf ${SAVE}
