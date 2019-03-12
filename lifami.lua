ef965316e6e64cfe38bfda7a3171adadeea1297d
Alex <alexandre.meyer@univ-lyon1.fr>
2019-03-12 11:02:57 +0100

    fix issue with make lifami

---
+++ lifami.lua
@@ -0,0 +1,14 @@
+
+-- Do not remove this line!
+dofile "./grapic.lua"
+dofile "./premake4.lua"
+
+
+make_project( "LIFAMI_1_Complex", "apps/LIFAMI/TPComplex.cpp" )
+make_project( "LIFAMI_2_Interpolation", "apps/LIFAMI/TPInterpolation.cpp" )
+make_project( "LIFAMI_3_Particle", "apps/LIFAMI/TPParticle.cpp" )
+make_project( "LIFAMI_4_ParticleSpring", "apps/LIFAMI/TPParticleSpring.cpp" )
+make_project( "LIFAMI_5_GameOfLife", "apps/LIFAMI/TPGameOfLife.cpp" )
+make_project( "LIFAMI_6_WolfRabbit", "apps/LIFAMI/TPWolfRabbit.cpp" )
+make_project( "LIFAMI_7_ColorInsects", "apps/LIFAMI/TPColorInsects.cpp" )
+make_project( "LIFAMI_8_IceScream", "apps/LIFAMI/TPIceScream.cpp" )
