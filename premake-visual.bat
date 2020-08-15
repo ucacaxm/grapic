06620b2c6a42652bf9065ce6e070fdac4ebfbf68
Anass 'Serdok' Lahnin <l.anass.pro@gmail.com>
2020-08-15 16:57:34 +0200

    :building_construction: Added SDL2 as a Git submodule

--- premake-visual.bat
+++
@@ -1,6 +0,0 @@
-
-del /S /Q build
-
-.\script\premake5.exe vs2015
-
-pause
--- premake4.lua
+++
@@ -1,9 +0,0 @@
-
--- Do not remove this line!
-dofile "./grapic.lua"
-
-
--- To add a new project, it is here
-
-make_project( "LIFAP1_TP3", "apps/LIFAP1/main_LIFAP1_TP3.cpp" )
--- make_project( "LIFAP1_TP7", "apps/LIFAP1/main_LIFAP1_TP7.cpp" )
