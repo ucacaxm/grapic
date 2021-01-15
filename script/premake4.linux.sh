ffbed0f02f61d728a2e2dad9f6b41e432aaa396d
Alex <alexandre.meyer@univ-lyon1.fr>
2021-01-15 13:22:32 +0100

    premake

--- script/premake4.linux.sh
+++ script/premake4.linux.sh
@@ -2,6 +2,8 @@
 
 if command -v premake4 > /dev/null 2>&1; then
     premake4 $*
+elif command -v script/premake4.linux > /dev/null 2>&1; then
+    script/premake4.linux $*
 else
     echo "Installer premake svp avec la commande : sudo apt install g++ premake4 zip doxygen libsdl2-dev libsdl2-mixer-dev libsdl2-image-dev libsdl2-ttf-dev"
 fi
