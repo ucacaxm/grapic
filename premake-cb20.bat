9cfe53d1760dda135a39dea9372981b8250033ae
Alex <alexandre.meyer@univ-lyon1.fr>
2023-11-28 17:43:28 +0100

    ...

--- premake-cb20.bat
+++ premake-cb20.bat
@@ -3,7 +3,7 @@ del /S /Q build
 
 .\script\premake5.exe --os=windows --file=premake5.lua --cb-version=cb20 codeblocks 
 .\script\premake5.exe --os=windows --file=premake5.lua gmake
-.\script\premake5.exe --os=windows --file=premake5.lua vscode
+@REM .\script\premake5.exe --os=windows --file=premake5.lua vscode
 
 copy extern\mingw-cb20\bin\*.dll bin
 
