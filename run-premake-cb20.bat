7189e695745831bfbb74ae00cf27a946f032fc97
Alex <alexandre.meyer@univ-lyon1.fr>
2023-11-30 08:17:39 +0100

    fix bin dir

--- run-premake-cb20.bat
+++ run-premake-cb20.bat
@@ -1,10 +1,17 @@
 
+@REM @ECHO OFF
+
+@echo "==================== del build ===================="
 del /S /Q build
 
+@echo "==================== premake CB ===================="
 .\script\premake5.exe --os=windows --file=premake5.lua --cb-version=cb20 codeblocks 
+@echo "==================== premake gmake ===================="
 .\script\premake5.exe --os=windows --file=premake5.lua --cb-version=cb20 gmake
-@REM .\script\premake5.exe --os=windows --file=premake5.lua vscode
 
+@echo "==================== copy dll =>bin ===================="
+dir
+mkdir bin
 copy extern\mingw-cb20\bin\*.dll bin
 
 pause
